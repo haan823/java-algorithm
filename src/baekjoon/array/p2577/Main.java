fe069f5052331ccfac47f38fa83be0f96a7d6e2f
SANGHYUN KIM <haan823@naver.com>
2022-04-12 09:55:32 +0900

    숫자의 개수

---
+++ src/baekjoon/array/p2577/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.array.p2577;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Long result = scanner.nextLong() * scanner.nextLong() * scanner.nextLong();
+        String s = result.toString();
+        int[] intArray = new int[10];
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            intArray[s.charAt(i) - '0'] += 1;
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+}

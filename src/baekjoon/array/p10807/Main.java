0840e3857b843d770610ce4b4ddb748644d72be6
SANGHYUN KIM <haan823@naver.com>
2022-04-12 11:40:18 +0900

    개수 세기

---
+++ src/baekjoon/array/p10807/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.array.p10807;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            list.add(scanner.nextInt());
+        }
+        int v = scanner.nextInt();
+        long count = list.stream().filter(i -> i.equals(v)).count();
+        System.out.println(count);
+    }
+}

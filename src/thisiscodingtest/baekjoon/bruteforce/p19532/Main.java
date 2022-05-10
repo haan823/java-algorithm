2836e9e3f2904a9ec5c6b7426513c7f8a52f7127
SANGHYUN KIM <haan823@naver.com>
2022-05-11 08:52:47 +0900

    수학은 비대면강의입니다

---
+++ src/thisiscodingtest/baekjoon/bruteforce/p19532/Main.java
@@ -0,0 +1,32 @@
+package thisiscodingtest.baekjoon.bruteforce.p19532;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        for (int x = -999; x <= 999; x++) {
+            for (int y = -999; y <= 999; y++) {
+                if (a * x + b * y == c && d * x + e * y == f) {
+                    bw.write(String.valueOf(x));
+                    bw.write(" ");
+                    bw.write(String.valueOf(y));
+                    bw.flush();
+                    br.close();
+                    bw.close();
+                }
+            }
+        }
+    }
+}

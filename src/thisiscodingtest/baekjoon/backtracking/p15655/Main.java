3b4b8b3854c9b40a36e20f002c699267110eeeb3
SANGHYUN KIM <haan823@naver.com>
2022-05-22 20:19:54 +0900

    N과 M(6)

---
+++ src/thisiscodingtest/baekjoon/backtracking/p15655/Main.java
@@ -0,0 +1,49 @@
+package thisiscodingtest.baekjoon.backtracking.p15655;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int[] array = new int[8];
+    public static boolean[] visited = new boolean[8];
+    public static int[] input = new int[]{10001, 10001, 10001, 10001, 10001, 10001, 10001, 10001, 10001};
+    public static int n;
+    public static int m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
+        recur(0, 0);
+    }
+
+    private static void recur(int cur, int start) {
+        if (cur >= 2) {
+            if (array[cur - 2] >= array[cur - 1]) {
+                return;
+            }
+        }
+        if (cur == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(array[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            if (visited[i]) continue;
+
+            array[cur] = input[i];
+            visited[i] = true;
+            recur(cur + 1, i + 1);
+            visited[i] = false;
+        }
+    }
+}

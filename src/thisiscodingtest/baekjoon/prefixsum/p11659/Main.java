f2ea8876d96993ea1ddb53c6907c997078d82855
SANGHYUN KIM <haan823@naver.com>
2022-07-03 12:08:07 +0900

    구간 합 구하기 4

---
+++ src/thisiscodingtest/baekjoon/prefixsum/p11659/Main.java
@@ -0,0 +1,29 @@
+package thisiscodingtest.baekjoon.prefixsum.p11659;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] array = new int[n + 1];
+        int[] prefix = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+            prefix[i] = prefix[i - 1] + array[i];
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(prefix[e] - prefix[s - 1]));
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}

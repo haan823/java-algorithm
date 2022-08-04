367e18ad7c82b7b6d28bbc0615aa8c82462e9d60
Sanghyun Kim <haan823@naver.com>
2022-08-04 09:10:41 +0900

    [Silver III] Title: N과 M (1), Time: 2728 ms, Memory: 63528 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] array = new int[110];
+    static boolean[] visited = new boolean[110];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        recur(0);
+        bw.flush();
+    }
+
+    private static void recur(int cur) {
+        if (cur == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(array[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            array[cur] = i;
+            visited[i] = true;
+            recur(cur + 1);
+            visited[i] = false;
+        }
+    }
+}

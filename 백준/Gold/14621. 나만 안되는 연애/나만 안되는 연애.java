cf345f8f0eef14eeb3ea83c39c96d65b12f057e9
Sanghyun Kim <haan823@naver.com>
2022-08-25 17:13:08 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 240 ms, Memory: 20712 KB -BaekjoonHub

--- 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.java
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.java
@@ -1,92 +1,79 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static int n;
-	public static int m;
-	public static char[] school;
-	public static int[] par;
-	public static int result;
-	public static boolean[] visited;
+    public static int n, m;
+    public static int[] par;
+    public static char[] array;
+    public static int result;
+    public static int cnt;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		school = new char[n + 1];
-		par = new int[n + 1];
-		visited = new boolean[n + 1];
-		for (int i = 0; i < n + 1; i++) {
-			par[i] = i;
-		}
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) {
-			school[i] = st.nextToken().charAt(0);
-		}
-		List<int[]> list = new ArrayList<>();
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			list.add(new int[] { a, b, c });
-		}
-		Collections.sort(list, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[2] - o2[2];
-			}
-		});
-		for (int i = 0; i < list.size(); i++) {
-			int a = list.get(i)[0];
-			int b = list.get(i)[1];
-			int c = list.get(i)[2];
-			if (school[a] == school[b]) {
-				continue;
-			}
-			if (find(a) == find(b)) {
-				continue;
-			}
-			visited[a] = true;
-			visited[b] = true;
-			union(a, b);
-			result += c;
-		}
-		boolean flag = true;
-		for (int i = 1; i <= n; i++) {
-			if (!visited[i]) {
-				flag = false;
-				break;
-			}
-		}
-		if (flag) {
-			bw.write(String.valueOf(result));
-		} else {
-			bw.write("-1");
-		}
-		bw.flush();
-		br.close();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        array = new char[n];
+        par = new int[n];
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            array[i] = st.nextToken().charAt(0);
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[2] - o2[2];
+            }
+        });
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken());
+            pq.add(new int[]{a, b, c});
+        }
+        while (!pq.isEmpty()) {
+            int a = pq.peek()[0];
+            int b = pq.peek()[1];
+            int c = pq.peek()[2];
+            pq.poll();
+            if (find(a) == find(b)) {
+                continue;
+            }
+            if (array[a] == array[b]) {
+                continue;
+            }
+            union(a, b);
+            cnt++;
+            result += c;
+        }
+        if (cnt == n - 1) {
+            bw.write(String.valueOf(result));
+        } else {
+            bw.write("-1");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
 
-	private static int find(int x) {
-		if (par[x] == x) {
-			return x;
-		} else {
-			return par[x] = find(par[x]);
-		}
-	}
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        par[x] = y;
+    }
 
-	private static void union(int x, int y) {
-		x = find(x);
-		y = find(y);
-		par[x] = y;
-	}
+    private static int find(int x) {
+        if (par[x] == x) {
+            return x;
+        }
+        return par[x] = find(par[x]);
+    }
 }
